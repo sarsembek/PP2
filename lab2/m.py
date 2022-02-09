6339bc2bd25949679bb1295a71a38a778fdc5a3c
Arman <ar_sarsembek@kbtu.kz>
2022-02-09 12:54:07 +0600

    updated

--- lab2/m.py
+++ lab2/m.py
@@ -1,10 +1,11 @@
-from datetime import datetime
 a_list=[]
 while True:
     a_str=input()
     if  a_str=='0':
         break
-    a_list.append(a_str)
-a_list.sort(key=lambda date: datetime.strptime(date,'%d %m %Y'))
+    d,m,y=a_str.split()
+    x=[y,m,d]
+    a_list.append(x)
+a_list=sorted(a_list)
 for x in range(len(a_list)):
-    print(a_list[x])
+    print(*a_list[x][::-1])
